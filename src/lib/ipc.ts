04bbff32f4a0e17d7ec79870d4497889c1aa8c8f
Hayes Barber <you@example.com>
2024-06-21 19:04:18 -0400

    convert to object

--- src/lib/ipc.ts
+++ src/lib/ipc.ts
@@ -1,29 +1,29 @@
 import { invoke } from "@tauri-apps/api/tauri";
 
-class Ipc {
-  public hide = async (): Promise<void> => {
+const Ipc = {
+  hide: async (): Promise<void> => {
     await invoke("hide");
-  };
+  },
 
-  public quit = async (): Promise<void> => {
+  quit: async (): Promise<void> => {
     await invoke("quit");
-  };
+  },
 
-  public getToggleUiAccelerator = async (): Promise<string> => {
+  getToggleUiAccelerator: async (): Promise<string> => {
     const accelerator: string = await invoke("get_toggle_ui_accelerator");
     return accelerator;
-  };
+  },
 
-  public getTheme = async (): Promise<string> => {
+  getTheme: async (): Promise<string> => {
     const theme: string = await invoke("get_theme");
     return theme;
-  };
+  },
 
-  public getCommands = () => {};
+  getCommands: () => {},
 
-  public commandSelected = async (i: number): Promise<void> => {
+  commandSelected: async (i: number): Promise<void> => {
     await invoke("command_selected", { i });
-  };
-}
+  },
+};
 
 export default Ipc;
