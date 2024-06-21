237d2283776c32551fdb840b5e856fdc4008a99d
Hayes Barber <you@example.com>
2024-06-21 19:46:13 -0400

    set up state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import useAsyncEffect from "./hooks/useAsyncEffect";
+import { GlobalProviderContext, GlobalState } from "./hooks/useGlobalState";
+import useKey from "./hooks/useKey";
+import useTheme from "./hooks/useTheme";
 import "./index.css";
+import Ipc from "./lib/ipc";
 
 const App: React.FC = () => {
-  return <h1>Hot Keys V2</h1>;
+  useTheme();
+  useKey("Escape", () => Ipc.hide());
+  const commands = useAsyncEffect(Ipc.getCommands, []);
+  const toggleUi = useAsyncEffect(Ipc.getToggleUiAccelerator, "");
+
+  const value: GlobalState = {
+    commands,
+    toggleUi,
+  };
+
+  return (
+    <GlobalProviderContext.Provider value={value}>
+      <h1>Hot Keys V2</h1>
+    </GlobalProviderContext.Provider>
+  );
 };
 
 export default App;
