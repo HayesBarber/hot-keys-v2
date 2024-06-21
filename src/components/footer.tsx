cea465d8503b33eae4a76b823ef81649e53c83b6
Hayes Barber <you@example.com>
2024-06-21 19:47:00 -0400

    add footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,25 @@
+import { PropsWithChildren } from "react";
+import { Button } from "./button";
+import { CommandShortcut } from "./command";
+
+export const FooterMain: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <div className="px-2">
+      <div className="flex justify-between items-center border-t h-[45px] bg-background">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export const FooterButton: React.FC<
+  PropsWithChildren<{ onClick: () => void }>
+> = ({ onClick, children }) => {
+  return (
+    <Button variant="ghost" onClick={onClick}>
+      <CommandShortcut className="flex items-center">
+        {children}
+      </CommandShortcut>
+    </Button>
+  );
+};
