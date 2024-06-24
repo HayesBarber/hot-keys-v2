75c23a52ccb9d890dd4102dd3ddadb3ce1012586
Hayes Barber <you@example.com>
2024-06-24 17:01:12 -0400

    add svg and remove lucide

--- src/components/command.tsx
+++ src/components/command.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { Command as CommandPrimitive } from "cmdk";
-import { Search } from "lucide-react";
 
 import { cn } from "../lib/utils";
 
@@ -25,7 +24,21 @@ const CommandInput = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div className="px-2">
     <div className="flex items-center border-b px-3" cmdk-input-wrapper="">
-      <Search className="icon" />
+      <svg
+        className="icon"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <circle cx="11" cy="11" r="8" />
+        <path d="m21 21-4.3-4.3" />
+      </svg>
       <CommandPrimitive.Input
         ref={ref}
         className={cn(
