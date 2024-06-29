97c7c99ec38c487c89f8aa128f616283718a5a2e
Hayes Barber <you@example.com>
2024-06-29 19:11:55 -0400

    reuse home dir

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -1,5 +1,6 @@
 use std::process::Command;
 use dirs::home_dir;
+use once_cell::sync::Lazy;
 
 pub fn toggle(w: tauri::Window) {
     let visible = w.is_visible().unwrap_or(false);
@@ -18,7 +19,13 @@ pub fn spawn_command(command: &str) {
     let _ = Command::new("sh").arg("-c").arg(command).spawn();
 }
 
+static HOME_DIR: Lazy<Option<String>> = Lazy::new(init_home_dir);
+
 pub fn get_home_dir() -> Option<String> {
+  HOME_DIR.clone()
+}
+
+fn init_home_dir() -> Option<String> {
   let mut home: String = match home_dir() {
     Some(dir) => dir.to_str().unwrap_or("").to_string(),
     None => "".to_string(),
