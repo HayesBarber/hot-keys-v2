e1987fc3064d2500a027c6d2852054bbea2d289f
Hayes Barber <you@example.com>
2024-06-29 11:12:17 -0400

    match_file_paths command

--- src-tauri/src/ipc.rs
+++ src-tauri/src/ipc.rs
@@ -1,3 +1,6 @@
+use dirs::home_dir;
+use glob::{glob, Paths};
+
 use crate::{models::ClientCommand, utils::{quit_app, spawn_command}, HOT_KEYS};
 
 #[tauri::command]
@@ -47,4 +50,50 @@ pub fn command_selected(i: usize) {
         Some(c) => spawn_command(&c.command),
         None => return,
     };
+}
+
+#[tauri::command]
+pub fn match_file_paths(base: &str) -> Vec<String> {
+    let base_dir: String = match base {
+        "~" | "/" => {
+            let home_dir = home_dir();
+            
+            match home_dir {
+                Some(home) => home.to_str().unwrap_or("").to_string(),
+                None => "".to_string(),
+            }
+        },
+        _ => base.to_string()
+    };
+
+    let pattern = match base_dir.ends_with("/") {
+        true => base_dir + "*",
+        false => base_dir + "/*",
+    };
+
+    let entries = glob(pattern.as_str());
+
+    match entries {
+        Ok(paths) => string_array_from_paths(paths),
+        Err(_) => vec![],
+    }
+}
+
+fn string_array_from_paths(paths: Paths) -> Vec<String> {
+    let mut suggestions: Vec<String> = Vec::new();
+
+    for entry in paths {
+        if let Ok(path) = entry {
+            let as_string = path.to_str();
+
+            let value = match as_string {
+                Some(v) => v.to_string() + if path.is_dir() {"/"} else {""},
+                None => continue,
+            }; 
+
+            suggestions.push(value);
+        }
+    }
+
+    suggestions
 }
