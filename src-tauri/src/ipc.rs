b606a5e19bb80ecabf4b0204bb3176e1a62cd581
Hayes Barber <you@example.com>
2024-07-06 12:30:52 -0400

    assign to empty array

--- src-tauri/src/ipc.rs
+++ src-tauri/src/ipc.rs
@@ -115,7 +115,7 @@ fn client_commands_from_paths(paths: Paths, home_dir: &String) -> Vec<ClientComm
         Err(_) => return suggestions,
     };
 
-    cached.clear();
+    *cached = vec![];
 
     let mut i = 0;
 
